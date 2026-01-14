9590de9dc6b59b11b7279ed9631d7c2512f86bad
Maxx_Qc <maxx_qc@hotmail.com>
2026-01-14 17:33:58 -0500

    upd

--- generateQrCode.js
+++ generateQrCode.js
@@ -1,15 +1,15 @@
-const qrcode = require("qrcode");
-const Jimp = require("jimp");
+const qrcode = require('qrcode');
+const Jimp = require('jimp');
 
-const websiteUrl = "https://www.zeffy.com/fr-CA/ticketing/02666740-8f49-4212-b2d0-ef910dc01a76";
-const logoPath = "C:/Users/Maxx_/Downloads/integrale_formats/1000x1000_blanc.png";
-const qrCodePath = "./output/qr_code.png";
+const websiteUrl = 'https://integrale.etsmtl.ca';
+const logoPath = 'C:/Users/Maxx_/Downloads/logo_contour.png';
+const qrCodePath = './output/qr_code.png';
 
 async function generateQRCodeWithLogo(data, logoPath, outputPath) {
   try {
     // Generate the QR code
     const qrCodeBuffer = await qrcode.toBuffer(data, {
-      errorCorrectionLevel: "H",
+      errorCorrectionLevel: 'H',
       scale: 100,
     });
 
@@ -17,20 +17,16 @@ async function generateQRCodeWithLogo(data, logoPath, outputPath) {
     const qrCode = await Jimp.read(qrCodeBuffer);
     qrCode.resize(1000, 1000);
 
-    // Load the logo
-    const logo = await Jimp.read(logoPath);
-    logo.resize(269, 269);
-
     // Create a new image to combine QR code and logo
     const combinedImage = await new Jimp(1000, 1000);
-    combinedImage.composite(qrCode, 0, 0).composite(logo, 500 - 1 - logo.bitmap.width / 2, 500 - 1 - logo.bitmap.height / 2);
+    combinedImage.composite(qrCode, 0, 0);
 
     // Save the final image
     await combinedImage.writeAsync(outputPath);
 
     console.log(`QR Code with logo generated and saved to ${outputPath}`);
   } catch (error) {
-    console.error("Error generating QR Code with logo:", error.message);
+    console.error('Error generating QR Code with logo:', error.message);
   }
 }
 
