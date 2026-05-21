cb7538dc732375e67ad5577d2386c2ae340944c2
Maxx_Qc <maxx_qc@hotmail.com>
2026-05-20 20:22:01 -0400

    add readme and revert code to contain logo

---
+++ README.md
@@ -0,0 +1,5 @@
+## Running the app
+
+1. Run command `npm install` to download all dependencies
+2. Open `generateQrCode.js` and edit the variables at the top of the file for the QR Code to generate.
+3. Run command `npm run start`
--- generateQrCode.js
+++ generateQrCode.js
@@ -1,33 +1,71 @@
-const qrcode = require('qrcode');
-const Jimp = require('jimp');
-
+// The link for the QR Code
 const websiteUrl = 'https://integrale.etsmtl.ca';
-const logoPath = 'C:/Users/Maxx_/Downloads/logo_contour.png';
+
+// The path to the logo image
+// Use undefined if you don't want to include a logo
+const logoPath = undefined; // './path/to/logo.png';
+
+// The path where the generated QR code will be saved
 const qrCodePath = './output/qr_code.png';
 
-async function generateQRCodeWithLogo(data, logoPath, outputPath) {
+// Margin around the QR code in pixels
+// Default 4
+const margin = 1;
+
+// Color of the QR code
+// Default black #000000
+const color = '#0061ff';
+
+// ############################
+// Do not edit
+// ############################
+const qrcode = require('qrcode');
+const { Jimp } = require('jimp');
+
+const generateQRCodeWithLogo = async (data, logoPath, outputPath) => {
   try {
     // Generate the QR code
     const qrCodeBuffer = await qrcode.toBuffer(data, {
       errorCorrectionLevel: 'H',
       scale: 100,
+      width: 1000,
+      height: 1000,
+      margin: margin,
+      color: {
+        dark: color,
+        light: '#FFFFFF',
+      },
     });
 
     // Convert the QR code Buffer to a Jimp image
     const qrCode = await Jimp.read(qrCodeBuffer);
-    qrCode.resize(1000, 1000);
+
+    // Load the logo
+    const logo = logoPath === undefined ? undefined : await Jimp.read(logoPath);
+
+    if (logo) {
+      logo.resize({ w: 269, h: 269 });
+    }
 
     // Create a new image to combine QR code and logo
-    const combinedImage = await new Jimp(1000, 1000);
+    const combinedImage = await new Jimp({ width: 1000, height: 1000 });
     combinedImage.composite(qrCode, 0, 0);
 
-    // Save the final image
-    await combinedImage.writeAsync(outputPath);
+    if (logo) {
+      combinedImage.composite(
+        logo,
+        500 - 1 - logo.bitmap.width / 2,
+        500 - 1 - logo.bitmap.height / 2,
+      );
+    }
 
-    console.log(`QR Code with logo generated and saved to ${outputPath}`);
+    // Save the final image
+    await combinedImage.write(outputPath);
+
+    console.log(`QR Code generated and saved to ${outputPath}`);
   } catch (error) {
-    console.error('Error generating QR Code with logo:', error.message);
+    console.error('Error generating QR Code:', error.message);
   }
-}
+};
 
 generateQRCodeWithLogo(websiteUrl, logoPath, qrCodePath);
